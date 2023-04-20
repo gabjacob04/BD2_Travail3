5e13aa0150c36b1ff780df8af95cd5842e1f6743
Alexis Lavertu <94461289+2134071@users.noreply.github.com>
2023-04-20 11:09:13 -0400

    essaye

--- BD2_Travail3/GererErreurDeConcurrence.cs
+++ BD2_Travail3/GererErreurDeConcurrence.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.Entity.Infrastructure;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -25,6 +26,36 @@ namespace BD2_Travail3
         private void txt_valeur_proposé_TextChanged(object sender, EventArgs e)
         {
 
+        }
+
+        private void GererErreurDeConcurrence_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_prendre_valeur_bd_Click(object sender, EventArgs e)
+        {
+            
+          /*  DbUpdateConcurrencyException dex;
+            if (nom_projet is changed)
+            {
+                var valeurNomCoursOG = dex.Entries.Single().GetDatabaseValues().GetValue<string>("nom_projet");
+                txt_valeur_changee_de_bd.Text = valeurNomCoursOG;
+            }
+            else
+            {
+                var valeurPondOG = dex.Entries.Single().GetDatabaseValues().GetValue<string>("description_projet");
+                txt_valeur_changee_de_bd.Text = valeurPondOG;
+            }
+                    var value = dex.Entries.Single().GetDatabaseValues();
+                    dex.Entries.Single().OriginalValues.SetValues(value);
+                    context.SaveChanges();
+                    MessageBox.Show("Le cour est toute décalisser fak on a le cours d'origine normal.");
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }*/
         }
     }
 }
